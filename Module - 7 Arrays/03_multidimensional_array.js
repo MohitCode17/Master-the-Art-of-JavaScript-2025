b86731a49de187bb7a7d3b4d26ec8dbc1fc6fd36
MohitCode17 <mohit.codes17@gmail.com>
2024-11-16 22:20:50 +0530

    add-module-06 and 07

---
+++ Module - 7 Arrays/03_multidimensional_array.js
@@ -0,0 +1,13 @@
+// 👉 What is Multidimensional Array ?
+
+// A multidimensional array is an arrays inside arrays.
+
+let salary = [
+  ["Mohit", 50000],
+  ["Rohan", 45800],
+  ["Akash", 20000],
+];
+
+console.log(salary);
+console.log(salary[1]); // ['Rohan', 45800]
+console.log(salary[1][0]); // Rohan
